8bd3814bede88b5c2146d11a5c8c7f90a9596e36
anasofich <castellanos_chopas@hotmail.com>
2021-08-30 23:34:57 -0500

    finished exercises

--- script.js
+++ script.js
@@ -13,25 +13,39 @@ function showOutput() {
     const output = input.substring(0, 1).toUpperCase() + input.substring(1);
     document.querySelector("#output").value = output;
   } else if (options == "opt2") {
-    let firstSpace = input.index(" ");
-    let name = input.substring(0, firstSpace);
-    let output = name.length;
+    const firstSpace = input.indexOf(" ");
+    const output = input.substring(0, firstSpace);
+    document.querySelector("#output").value = output;
+  } else if (options == "opt3") {
+    const firstSpace = input.indexOf(" ");
+    const firstName = input.substring(0, firstSpace);
+    const output = firstName.length;
+    document.querySelector("#output").value = output;
+  } else if (options == "opt4") {
+    const firstSpace = input.indexOf(" ");
+    const lastSpace = input.lastIndexOf(" ");
+    const middleName = input.substring(firstSpace + 1, lastSpace);
+    const output = `Start position: ${firstSpace + 1}, end position: ${lastSpace - 1}, middle name: ${middleName}`;
+    document.querySelector("#output").value = output;
+  } else if (options == "opt5") {
+    if (input.endsWith(".jpg")) {
+      const output = "the file is a .jpg";
+      document.querySelector("#output").value = output;
+    } else if (input.endsWith(".png")) {
+      const output = "the file is a .png";
+      document.querySelector("#output").value = output;
+    }
+  } else if (options == "opt6") {
+    const len = input.length;
+    const output = "*";
+    document.querySelector("#output").value = output.repeat(len);
+  } else if (options == "opt7") {
+    const output = input.substring(0, 2).toLowerCase() + input.substring(2, 3).toUpperCase() + input.substring(3).toLowerCase();
+    document.querySelector("#output").value = output;
+  } else if (options == "opt8") {
+    const space = input.indexOf(" ");
+    const hyphen = input.indexOf("-");
+    const output = input.substring(0, space).toLowerCase() + input.substring(space + 1, space + 2).toUpperCase() + input.substring(space + 2).toLowerCase();
     document.querySelector("#output").value = output;
   }
-
-  /* if (options == "opt1") {
-    let part1 = input.substring(0, 1).toUpperCase();
-    let part2 = input.substring(1);
-    document.querySelector("#output").value = `${part1}${part2}`;
-  } */
-
-  //document.querySelector("#output").value = input;
 }
-
-/* function showOutput() {
-  const input = document.querySelector("#input").value;
-
-  console.log(input);
-
-  document.querySelector("#output").value = input;
-} */
